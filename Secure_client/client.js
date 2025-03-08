7899bf971dd509a9a36c8df4df19b0278e530ef9
SuperExago <fake@email.com>
2025-03-08 13:30:36 -0500

    Added secure version (going to add lgoin later)

---
+++ Secure_client/client.js
@@ -0,0 +1,95 @@
+
+// Local User Stuff
+const username = localStorage.getItem("username");
+const chatroom = localStorage.getItem("chatroom");
+
+if (username == null || chatroom == null) {
+        window.location.href = "index.html";
+}
+
+//err erError message console.log("hi");
+
+const webSocket = new WebSocket("wss://localhost:4433")
+
+// Webserver Stuff
+const messagesDiv = document.getElementById('message_box');
+const usersDiv = document.getElementById('users_box');
+const user_input = document.getElementById("input_box");
+
+document.addEventListener("DOMContentLoaded", function(event){
+        document.getElementById("room_name").innerHTML = chatroom;
+        user_input.addEventListener("input", function () {
+          // Reset the height to allow for recalculating
+          this.style.height = 'auto';
+          // Set the height to match the scrollHeight (content height)
+          this.style.height = this.scrollHeight + 'px';
+
+          // Check if the height exceeds max-height
+          const maxHeight = window.getComputedStyle(this).maxHeight;
+          if (parseInt(this.style.height) > parseInt(maxHeight)) {
+            this.style.height = maxHeight; // Set to max-height if exceeded
+          }
+        });
+});
+
+document.addEventListener("keypress", function(event){
+        if (event.key == "Enter" && !event.shiftKey) {
+                event.preventDefault();
+                submit_message();
+        }
+})
+
+webSocket.onopen = (event) => {
+        let msg = {username:username, type:"join", data:chatroom};
+        webSocket.send(JSON.stringify(msg));
+        console.log("connected to server")
+}
+
+webSocket.onmessage = (event) => {
+        // message: {timestamp, username, messsageType, message}
+        const data = JSON.parse(event.data);
+        switch (data["type"]) {
+                case "msg":
+                        addMessage(data);
+                        break;
+                case "error":
+                        let error_message = data["data"];
+                        //TODO Figure out how to redirect user back, you do this
+                        break;
+        }
+        
+        console.log(data);
+}
+
+function formatMessage(message) {
+        // Replace shortcuts with HTML tags
+        return message
+            .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>') // Bold
+            .replace(/~~(.*?)~~/g, '<span style="text-decoration: line-through;">$1</span>') // Strikethrough
+            .replace(/\*(.*?)\*/g, '<em>$1</em>') // Italics
+            .replace(/~(.*?)~/g, '<sub>$1</sub>') // Subscript
+            .replace(/\^(\S+)\^/g, '<sup>$1</sup>') // Superscript
+            .replace(/https?:\/\/[^\s]+/g, '<a href="$&" target="_blank">$&</a>') // Links
+            .replace(/\\n/g,"</br>");  // New Line
+}
+
+function addMessage(data) {
+        // ARGS: json {timestamp, username, message}
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('msg');
+        messageElement.innerHTML = ` <div class="msg-info"> <span class="msg-user">${data["username"]}</span> <span class="msg-time">${data["timestamp"]}</span> </div> ${formatMessage(data["data"])} `;
+        messagesDiv.appendChild(messageElement);
+        window.scrollTo(0, document.body.scrollHeight);
+}
+
+document.getElementById("submit").addEventListener("click", function(event){
+        submit_message();
+});
+
+function submit_message() {
+        if (user_input.value != "" && user_input.value.trim()!=""){
+                webSocket.send(JSON.stringify({username:username, type:"msg", data:user_input.value.trim()}));
+        }
+        user_input.value = "";
+        user_input.style.height = 'auto';
+}
---
+++ Secure_server/server.js
@@ -0,0 +1,77 @@
+const WebSocket = require("ws");
+
+// Server connection to 8080
+const wss = new WebSocket.Server({
+    port: 4433
+});
+
+// Constants
+const rooms = new Map();
+// { 'room1' (ROOM_NAME): {'users': [ <String>user: ws ], 'msg': []} }
+
+// Server Stuff
+wss.on("connection", function connection(ws) {
+    // START of INITAL CONNECTION
+    let username = '';
+    let room = '';
+
+    // END of INITAL CONNECTION
+    ws.on("error", console.error);
+    ws.on("message", function message(data) {
+        const msg = JSON.parse(data);
+        console.log(msg);
+        switch (msg["type"]) {
+            case "msg":
+                handle_message(username, room, data);
+                break;
+            case "join":
+                allowed_user = true;
+                username = msg["username"];
+                room = msg["data"];
+                room_map = rooms.get(room);
+                if (room_map != undefined) {
+                    users = room_map.get("users");
+                    if (users.get(username) != undefined) {
+                        ws.send(JSON.stringify({type: "error", data: "not unique user"}));
+                        ws.close();
+                        allowed_user = false;
+                    } 
+                }
+                if (allowed_user) {
+                    room_join_and_creation(username, ws, room);
+                }
+                break;
+        }
+    });
+
+    ws.on('close', () => {
+        if (username != '' && room != '') {
+            rooms.get(room).get("users").delete(username);
+        }
+        ws.close();
+        console.log(username + " disconnected");
+    });
+
+});
+
+function room_join_and_creation(username, ws, room_name) {
+    let room = rooms.get(room_name);
+    if (room == undefined) {
+        rooms.set(room_name, new Map([["users", new Map()], ["msg", []]]));
+        room = rooms.get(room_name);
+    }
+    let users = room.get("users");
+    users.set(username, ws);
+}
+
+function handle_message(username, room, msg_data) {
+    let chat_room = rooms.get(room);
+    const msg = JSON.parse(msg_data).data;
+    if (chat_room != undefined) {
+        let users = chat_room.get("users");
+        for (const [key, value] of users) {
+            value.send(JSON.stringify({username: username, type: "msg", data: msg, timestamp: new Date().toLocaleString()}));
+        }
+    }
+}
+
