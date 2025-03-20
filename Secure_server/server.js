7c145d9ffd6e9982ab8eb90d6e583a66906d38ed
SuperExago <fake@email.com>
2025-03-19 20:00:02 -0400

    Added foward message and add user

--- Secure_server/server.js
+++ Secure_server/server.js
@@ -6,9 +6,8 @@ const config = JSON.parse(fs.readFileSync("config.json"));
 const {v4: uuidv4} = require('uuid');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {decode} = require("punycode");
 
-const server_name = "server"; 
+const server_name = "server";
 
 const saltRounds = 10;
 
@@ -40,34 +39,88 @@ wss.on("connection", function connection(ws) {
     ws.on("message", function message(data) {
         console.log(data);
         if (user.key == undefined) {
-            const inital_data = JSON.parse(privateKey.decrypt(data));
-            user.iv = inital_data.iv
-            user.key = inital_data.key
-            user.cipher = forge.cipher.createCipher('AES-CBC', key);
-            user.decipher = forge.cipher.createDecipher('AES-CBC', key);
-            user.uid = uuidv4();
-            console.log(user);
+            try {
+                intialize_user(data.data, user);
+            } catch {
+                ws.send(JSON.stringify({user: server_name, type: "error", data: {msg: "Unexpected error, closing connection"}}));
+                ws.close();
+            }
         } else {
             const msg = decrypt(data);
+            const msg_data = JSON.parse(msg.data);
             switch (msg.type) {
                 case "login":
-                    user_login(JSON.parse(msg.data), user);
+                    user_login(msg_data, user);
                     break;
                 case "create":
-                    user_create(JSON.parse(msg.data), user);
+                    user_create(msg_data, user);
                     break;
                 case "jwt":
-                    user_auth(JSON.parse(msg.data), user);
+                    user_auth(msg_data, user);
                     break;
+                case "join_room":
+                    join_room(msg_data, user);
+                    break;
+                case "message":
+                    foward_message(msg_data, user);
+                    break;
+                default:
+                    console.log("Error, unkown message:"+data);
             }
         }
 
     });
 
     ws.on('close', () => {
-
+        if (user.room != undefined) {
+            rooms.get(user.room).delete(user.uid);
+        }
     });
 });
+
+function foward_message(data, user) {
+    const room = rooms.get(user.room);
+    if (room != undefined) {
+        for (const [key, value] of room) {
+            if (key !== user.uid) {
+                sendLogged(value, {username: user.username, type: "msg", data: {timestamp: data.timestamp, msg: data.msg}});
+            }
+        }
+    } else {
+        sendLogged(user, {username: server_name, type: "error", data: {detail: "foward_message", msg: "Room does not exist, closing connection"}});
+        user.ws.close();
+    }
+}
+
+function intialize_user(data, user) {
+    const inital_data = JSON.parse(privateKey.decrypt(data));
+    user.iv = inital_data.iv
+    user.key = inital_data.key
+    user.cipher = forge.cipher.createCipher('AES-CBC', key);
+    user.decipher = forge.cipher.createDecipher('AES-CBC', key);
+    user.uid = uuidv4();
+    console.log(user);
+}
+
+function join_room(data, user) {
+    const room_name = data.room;
+    let room = rooms.get(room_name);
+    if (room == undefined) {
+        room = new Map();
+        rooms.set(room_name, room);
+        room.set(user.uid, user);
+        sendLogged(user, {username: server_name, type: "joined", data: {msg: "Sucessfully joined room " + room_name}});
+    } else if (room.has(user.uid)) {
+        sendLogged(user, {username: server_name, type: "error", data: {detail: "user_create", msg: "Duplicate user in room, closing connection"}});
+        user.ws.close();
+    } else {
+        for (const [key, value] of room) {
+            sendLogged(value, {username: server_name, type: "user_add", data: {username: user.username}});
+        }
+        room.set(user.uid, user);
+        sendLogged(user, {username: server_name, type: "joined", data: {msg: "Sucessfully joined room " + room_name}});
+    }
+}
 
 function user_auth(data, user) {
     const token = data.token;
@@ -92,7 +145,7 @@ function user_create(data, user) {
     const pattern = new RegExp("\\W");
 
     if (pattern.test(username)) {
-        sendLogged(user, {username: server_name, type: "error", data: {detail:"user_create", msg:"Invalid username, username can only contain alphanumeric characters"}});
+        sendLogged(user, {username: server_name, type: "error", data: {detail: "user_create", msg: "Invalid username, username can only contain alphanumeric characters"}});
     } else {
         db.get("SELECT * FROM users WHERE username = ?", (username), function (err, row) {
             query_result = row;
@@ -102,9 +155,9 @@ function user_create(data, user) {
             user.username = data.username;
             send_token(user);
         } else {
-            sendLogged(user, {username: server_name, type: "error", data: {detail:"user_create", msg:"Error, user already exists, please login or choose a different username"}});
+            sendLogged(user, {username: server_name, type: "error", data: {detail: "user_create", msg: "Error, user already exists, please login or choose a different username"}});
         }
-    }    
+    }
 }
 
 function user_login(data, user) {
@@ -127,7 +180,7 @@ function user_login(data, user) {
         user.username = user_data.username;
         send_token(user);
     } else {
-        sendLogged(user, {username: server_name, type: "error", data: {detail:"user_login", msg:"Error, wrong username or password, please try again"}});
+        sendLogged(user, {username: server_name, type: "error", data: {detail: "user_login", msg: "Error, wrong username or password, please try again"}});
     }
 }
 
@@ -138,14 +191,6 @@ function hash_password(password) {
         return hash;
     });
 }
-
-function encrypt(data, cipher, iv) {
-    cipher.start({iv: iv});
-    cipher.update(forge.util.createBuffer(data));
-    cipher.finish();
-    return cipher.output.getBytes();
-}
-
 function decrypt(data, decipher, iv) {
     decipher.start({iv: iv});
     decipher.update(data);
@@ -153,7 +198,11 @@ function decrypt(data, decipher, iv) {
 }
 
 function sendLogged(user, message) {
-    user.ws.send(encrypt(message));
+    const cipher = user.cipher;
+    cipher.start({iv: user.iv});
+    cipher.update(forge.util.createBuffer(JSON.stringify(message)));
+    cipher.finish();
+    user.ws.send(cipher.output.getBytes());
     console.log(message);
 }
 
